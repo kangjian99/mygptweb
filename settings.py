737e9b4a2ca0555843f88652592efb982fb4a759
kangjian99 <radiokj@163.com>
2023-11-07 11:07:56 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ import openai
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 model = 'gpt-3.5-turbo-1106' # or text-davinci-003
 model_4 = 'gpt-4-1106-preview'
+
 SESSION_SECRET_KEY = os.environ.get('SESSION_SECRET_KEY')
 
 server = 'tcp:kj99.database.windows.net,1433'
