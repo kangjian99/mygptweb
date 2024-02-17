8c19ca92925ee4403d13983e19a6224f67d8397c
kangjian99 <radiokj@163.com>
2024-02-17 12:03:47 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ model_4 = 'gpt-4-1106-preview'
 SESSION_SECRET_KEY = os.environ.get('SESSION_SECRET_KEY')
 
 server = 'tcp:kj99.database.windows.net,1433'
-database = 'database'
+database = 'db'
 db_username = os.environ.get('DB_USERNAME')
 db_password = os.environ.get('DB_PASSWORD')
 driver = '{ODBC Driver 18 for SQL Server}'
